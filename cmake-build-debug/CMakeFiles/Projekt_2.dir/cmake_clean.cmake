eff13994b01a75457e4cdcfb9153ef81fba59010
Tomas Berezny <tberezny552@gmail.com>
2020-11-24 21:00:51 +0100

    push vsetko

---
+++ cmake-build-debug/CMakeFiles/Projekt_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt_2.dir/main.c.obj"
+  "Projekt_2.exe"
+  "Projekt_2.exe.manifest"
+  "Projekt_2.pdb"
+  "libProjekt_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projekt_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
